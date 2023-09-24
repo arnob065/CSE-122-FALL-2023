d6503ccc9dbad00e79cec8bb8f98975b5b181f1a
Tamjid faiaz arnob <141550069+arnob065@users.noreply.github.com>
2023-09-24 16:04:44 +0600

    Update Boys and girls.cpp

--- LAB04/Boys and girls.cpp
+++ LAB04/Boys and girls.cpp
@@ -1,23 +1,24 @@
 #include<iostream>
-#include<algorithm>
-#include<string>
+#include<string.h>
 using namespace std;
-int main()
-{
-   string s;
-   int i;
-   int count=0;
-   cin>>s;
-   sort(s.begin(),s.end());
-   for(i=0;i<s.size();i++)
-   {
-      if(s[i]!=s[i+1])
-      {
-         count++;
-      }
-   }
-   if(count%2==0)
-      cout<<"CHAT WITH HAR!";
-   else
-      cout<<"IGNORE HIM!";
+int main(){
+char x[100];
+cin>>x;
+int count=0;
+    for(int i=0;i<100;i++){
+        if(x[i]!=x[i+1]){
+            count++;
+        }
+        else{
+            count+=1;
+        }
+    }
+    if(count%2!=1){
+
+    cout<<"CHAT WITH HER!";
+
+     }
+
+else{cout<<"IGNORE HIM!";}
+
 }
